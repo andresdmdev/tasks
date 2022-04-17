94e3287bdc3f6cd285a901b47909b2095a5908a8
Andres Marquez <andresmftech5@gmail.com>
2022-04-17 15:08:02 -0500

    TODO LISTO

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import logo from './img/amazon-logo.png';
+import Lista from './componentes/lista';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="aplicacion-tareas">
+      <div className='contenedor-logo'>
+        <img 
+          src={logo} 
+          className='amazon-logo' />
+      </div>
+      <div className='lista-principal'>
+        <h1>Mis Tareas</h1>
+        <Lista />
+      </div>
     </div>
   );
 }
